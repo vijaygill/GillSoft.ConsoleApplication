bf2dad259f0dac9ca855d03f41e5040b08814da9
Vijay Gill <vijay.gill@hotmail.com>
2017-10-28 14:47:28 +0100

    Added XML documentation.

--- GillSoft.ConsoleApplicationFramework/IOutput.cs
+++ GillSoft.ConsoleApplicationFramework/IOutput.cs
@@ -11,8 +11,22 @@ namespace GillSoft.ConsoleApplicationFramework
     /// </summary>
     public interface IOutput
     {
+        /// <summary>
+        /// Write a message in normal mode.
+        /// </summary>
+        /// <param name="message"></param>
         void WriteLine(string message);
+
+        /// <summary>
+        /// Write a message in error mode.
+        /// </summary>
+        /// <param name="message"></param>
         void WriteError(string message);
+
+        /// <summary>
+        /// Write a message in warning mode.
+        /// </summary>
+        /// <param name="message"></param>
         void WriteWarning(string message);
     }
 }
